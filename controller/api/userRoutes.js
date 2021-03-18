2b7f4edd72147edc9b20d2d50001f80823d275d4
Connor Walden <connormwalden@gmail.com>
2021-03-18 20:31:25 +1030

    Finished Crud for user api routes

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -3,6 +3,16 @@ const User = require('../../models/User');
 
 // CREATE
 
+router.post('/', async (req, res) => {
+    // find all users    
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json(user);  
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 // READ
 
 // get all users
@@ -17,7 +27,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    // find all users    
     try {
         const user = await User.findByPk(req.params.id);
         res.status(200).json(user);  
@@ -28,4 +37,22 @@ router.get('/:id', (req, res) => {
 
 // UPDATE
 
+router.put('/:id', async (req, res) => {
+    try {
+        const user = await User.update(req.body, { where: { id: req.params.id } });
+        res.status(200).json(user);  
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 // DELETE
+
+router.delete('/:id', async (req, res) => { 
+    try {
+        const user = await User.destroy({ where: { id: req.params.id }});
+        res.status(200).json(user);  
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
