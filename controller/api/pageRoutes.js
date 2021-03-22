4fc55cd8aeea30594f512dad32477c5e607f622c
Connor Walden <connormwalden@gmail.com>
2021-03-22 18:11:42 +1030

    Full CRUD complete for page routes

--- controller/api/pageRoutes.js
+++ controller/api/pageRoutes.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const { Router } = require('express');
-const Page = require('../../models/Page');
-const PageData = require('../../models/PageData');
+const { Page, PageData, User } = require('../../models');
 
 // CREATE
-
 router.post('/', async (req, res) => {
     const pageInfo = { name: req.body.name, description: req.body.description };
 
@@ -35,26 +33,141 @@ router.post('/', async (req, res) => {
 
 });
 
-// READ
-
-router.get('/', async (req, res) => {
+// Create data for a page using id
+router.post('/:id', async (req, res) => {
     try {
-        const page = await Page.findAll({
-            include: {
-                model: PageData
-            }
+        const pd = await PageData.create({
+            page_id: req.params.id,
+            heading: req.body.heading,
+            data: req.body.data,
+            is_reply: req.body.is_reply,
+            reply_user_id: null
         });
 
-        res.status(200).json(page);
+        res.status(200).json(pd);
     }
     catch(err) {
         res.status(500).json(err);
     }
 });
 
+// READ
+
+// Get all
+router.get('/', async (req, res) => {
+    try {
+        const page = await Page.findAll({ include: PageData });
+
+        res.status(200).json(page);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Get one
+router.get('/:id', async (req, res) => {
+    try {
+        const page = await Page.findByPk(req.params.id, {
+            include: PageData
+        });
+
+        if(page == null) {
+            res.status(404).json({ message: "Could not find page with id: " + req.params.id });
+            return;
+        }
+
+        res.status(200).json(page);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 // UPDATE
 
+router.put('/:id', async (req, res) => {
+    try {
+        const page = await Page.update(req.body, { 
+            where: { 
+                id: req.params.id 
+            }
+        });
+
+        if(page == null) {
+            res.status(404).json({ message: "Could not find page with id: " + req.params.id });
+            return;
+        }
+
+        res.status(200).json(page);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id/data/:data_id', async (req, res) => {
+    try {
+        const page = await PageData.update(req.body, { 
+            where: { 
+                id: req.params.data_id,
+                page_id: req.params.id 
+            }
+        });
+
+        if(page == null) {
+            res.status(404).json({ message: "Could not find page with id: " + req.params.id });
+            return;
+        }
+
+        res.status(200).json(page);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 // DELETE
+// Delete page
+router.delete('/:id', (req, res) => {
+    try {
+        const page = Page.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
+
+        if(page == null) 
+            res.status(404).json({ message: "Page not found with id: " + req.params.id })
+
+        res.status(200).json(page);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Delete page data
+router.delete('/:id/data/:data_id', async (req, res) => {
+    try {
+        const page = await PageData.destroy({ 
+            where: { 
+                id: req.params.data_id,
+                page_id: req.params.id 
+            }
+        });
+
+        if(page == null) {
+            res.status(404).json({ message: "Could not find page with id: " + req.params.id });
+            return;
+        }
+
+        res.status(200).json(page);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
