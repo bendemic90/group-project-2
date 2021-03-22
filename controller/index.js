97484703ea77ed97bd320e164cc3e09967350006
Ben T <ben.trevethan@gmail.com>
2021-03-22 19:11:48 +1030

    basic handlebars engine template, session and homepage routing

--- controller/index.js
+++ controller/index.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
+const { User } = require('../models');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    res.send("<h1>Wrong Route!</h1>")
+// router.use((req, res) => {
+//     res.send("<h1>Wrong Route!</h1>")
+// });
+
+router.get('/', async (req, res) => {
+    try {
+        const tempPageStuff = await User.findAll();
+        res.render('homepage', tempPageStuff);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require('express');
-//const session = require('express-session');
+const session = require('express-session');
 const routes = require('./controller');
-//const exphbs = require('express-handlebars');
-//const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-// const sess = {
-//   secret: 'Dc0d3',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Dc0d3',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
  
-//app.use(session(sess));
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
